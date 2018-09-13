8a5608fd70be15bac551e5f285bb5bfb53070f84
Caroline McCormick <cmccormick1@email.arizona.edu>
2018-09-13 15:52:29 -0700

    masked OH lines on spectra

--- library.py
+++ library.py
@@ -9,14 +9,22 @@ import glob, pdb, string
 
 
 
-def stack_spectra(fname, plot = False, indices = 'placeholder'):
+def stack_spectra(fname, mname='', plot = False, indices = 'placeholder'):
     hdu = fits.open(fname)
-    temp1 = fname.split('/')
-    temp2 = temp1[1].split('.')
-    field = temp2[0]
+    #temp1 = fname.split('/')
+    #temp2 = temp1[1].split('.')
+    #field = temp2[0]
     image = hdu[0].data
     header = hdu[0].header
     wavelen = header["CRVAL1"] + np.array(header["CDELT1"])*range(header['NAXIS1'])
+    
+    if mname != '':
+        hdu = fits.open(mname)
+        mask_image = hdu[0].data
+        mask_header = hdu[0].header
+        
+        image = np.ma.array(image, mask=mask_image)
+    
     if (indices == 'placeholder'):
         flux = np.mean(image, axis = 0)
     else:
@@ -25,7 +33,7 @@ def stack_spectra(fname, plot = False, indices = 'placeholder'):
         plt.plot(wavelen, flux)
         plt.xlabel("Wavelength")
         plt.ylabel("Flux")
-        plt.title("Stacked Spectra of %s" % field)
+        plt.title("Stacked Spectra of %s" % fname)
     
     hdu.close()
     
@@ -38,17 +46,19 @@ def stack_spectra(fname, plot = False, indices = 'placeholder'):
 
 
 
-def binning(temp_x, bin_pts, spectra_plot = False, filename = False):
+def binning(temp_x, bin_pts, mname = '', spectra_plot = False, filename = False):
     """
     temp_x = quantity to be divided into bins [must NOT be sorted]
     bin_pts = Number of points in each bin
     """
+   
     
     temp_x1 = np.sort(temp_x)
     temp_ind = np.argsort(temp_x)
     temp_ind1 = np.where((np.isfinite(temp_x1)==True) & (temp_x1>0) & (temp_x1 < 1e13))[0]
     x = np.log10(temp_x1[temp_ind1])
     ind = temp_ind[temp_ind1]
+    print(len(x))
     y = range(len(x))
     
     start = 0
@@ -70,18 +80,21 @@ def binning(temp_x, bin_pts, spectra_plot = False, filename = False):
         bin_edge.append(bin_start)
         bin_size.append((bin_stop - bin_start))
         if filename != False:
-            _, flx, wave = stack_spectra(filename, indices = ind[start:stop])
+            #print(start, stop, ind[start], ind[stop])
+            #if start == 0: print(np.log10(temp_x[ind[start:stop]]))
+            _, flx, wave = stack_spectra(filename, mname, indices = ind[start:stop])
             flux.append(flx)
             wavelength.append(wave)
         start, bin_start = stop, bin_stop
     
     if (spectra_plot == True):
         for i in range(count):
-            plt.subplot(count/2, 2, i+1)
+            plt.subplot(np.ceil(count/2.0), 2, i+1)
             plt.plot(wavelength[i], flux[i])
-            plt.ylim(-0.2e-17, 2.0e-17)
-            plt.axvline(x=5007, color='k', linestyle = 'dashed')
-            plt.axvline(x=4363, color='r', linestyle = 'dashed')
+            plt.ylim(-0.05e-17, 2.0e-17)
+            plt.xlim(4700,5100)
+            plt.axvline(x=5007, color='k', linestyle = 'dashed', alpha=0.5)
+            plt.axvline(x=4363, color='r', linestyle = 'dashed', alpha=0.5)
     else:
         plt.bar(bin_edge, distribution, align = 'edge', width = bin_size)
         
