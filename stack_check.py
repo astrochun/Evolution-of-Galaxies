3d0b987c2b64e0c4e7904e0e3001dfea4f1326fa
Caroline McCormick <cmccormick1@email.arizona.edu>
2018-09-27 16:31:22 -0700

    calculated sigma and plotted histrogram of sigma values in sigma array

--- stack_check.py
+++ stack_check.py
@@ -22,10 +22,15 @@ def stack_check(fname, bin_array_file, out_pdf, mname=''):
         mask_image = hdu[0].data
         mask_header = hdu[0].header
         
+        zero_x, zero_y = np.where(image == 0)
+        mask_image[zero_x, zero_y] = 1
+        
         image = np.ma.array(image, mask=mask_image)
     
+    sigma_array = np.zeros(len(indices))
+    
     pp = PdfPages(out_pdf)
-    for ii in range(10): #len(indices)):
+    for ii in range(len(indices)):
         row = np.int(np.floor((ii/ncols) % nrows))
         if ii % (nrows*ncols) == 0:
             fig, ax = plt.subplots(nrows=10, ncols=1)
@@ -37,10 +42,16 @@ def stack_check(fname, bin_array_file, out_pdf, mname=''):
         ax[row].axvline(x=3728.91, color="black", linestyle="dashed", linewidth=0.5)
         ax[row].axvline(x=4861.32, color="black", linestyle="dashed", linewidth=0.5)
         ax[row].plot(wavelength, flux/1e-17)
+        
         ax[row].set_xlabel("Wavelength")
         ax[row].set_ylabel("Flux")
         ax[row].set_xlim(3700, 5300)
         #ax[row].set_title("Stacked Spectra of %s" % fname)
+        
+        sigma_array[ii] = np.ma.std(flux/1e-17)
+        ax[row].annotate('sigma = %.3f' % sigma_array[ii], [0.95,0.95], xycoords='axes fraction',
+                         ha='right', va='top')
+
         
         if (ii % (nrows*ncols) == nrows*ncols-1) or (ii == len(indices)):
             subplots_adjust(left=0.05, bottom=0.05, top=0.99, right=0.99, hspace=0.03)
@@ -49,4 +60,7 @@ def stack_check(fname, bin_array_file, out_pdf, mname=''):
             
     pp.close()
     hdu.close()
-        
+    
+    fig, ax = plt.subplots()
+    ax.hist(sigma_array, bins=50)
+    fig.savefig(out_pdf.replace(".pdf", "_hist.pdf"))
