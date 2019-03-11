f9721a01433cc52b818cadcde25f528833de2ac4
Caroline McCormick <cmccormick1@email.arizona.edu>
2019-03-11 11:04:31 -0700

    Included an option for adaptive binning

--- library.py
+++ library.py
@@ -1,10 +1,20 @@
 from astropy.io import fits, ascii
-from astropy.table import Table
+from astropy.table import Table, hstack
 from collections import Counter
 import numpy as np
 import matplotlib.pyplot as plt
 import glob, pdb, string
+from getpass import getuser
 
+
+
+if getuser() == 'carol':
+    path = "C:\\Users\\carol\\Google Drive\\MZEvolve\\"
+    path2 = path
+else:
+    path = "../DEEP2/" 
+    path2 = "../"
+    
 
 def exclude_outliers(objno):
     flag = np.zeros(len(objno), dtype=int)
@@ -49,63 +59,75 @@ def stack_spectra(fname, mname='', plot = False, indices = 'placeholder'):
 
 def interpolate_data(interp_file):
     npz_file = np.load(interp_file)
-    interp_data, mag_no_mass, no_mass_idx = npz_file['interp_data'], npz_file['mag_no_mass'], npz_file['no_mass_idx']
+    interp_data, mag_no_mass, no_mass_idx = npz_file['interp_data'][0], npz_file['mag_no_mass'], npz_file['no_mass_idx']
     interp_mass = interp_data(mag_no_mass)
+    
+    plt.scatter(mag_no_mass, interp_mass)
+    
     return interp_mass, no_mass_idx
     
 
 
-def binning(temp_x, objno, bin_pts, interp_file, mname = '', bin_array_file = '', spectra_plot = False,
-            filename = False):
+def binning(temp_x, objno, bin_pts_input, interp_file, bin_pts_fname, mname = '', bin_array_file = '',
+            spectra_plot = False, filename = False, adaptive = False):
     """
     temp_x = quantity to be divided into bins [must NOT be sorted]
-    bin_pts = Number of points in each bin
+    bin_pts_input = Number of points in each bin
     """
     
-    interp_mass, no_mass_idx = interpolate_data(interp_file)
+    interp_mass, no_mass_idx = interpolate_data(interp_file)    
+    interp_mass = 10**(interp_mass)
     temp_x[no_mass_idx] = interp_mass
     
-    temp_x1 = np.sort(temp_x)
-    temp_ind = np.argsort(temp_x)
-    objno = objno[temp_ind]
+    x_sort = np.sort(temp_x)
+    ind_sort = np.argsort(temp_x)
+    objno = objno[ind_sort]
 
     temp_flag = exclude_outliers(objno)
+    
 
     #Remove bad data and initially excluded data
-    temp_ind1 = np.where((np.isfinite(temp_x1)==True) & (temp_x1>0) & (temp_x1 < 1e13) & (temp_flag == 0))[0]
-    #temp_ind2 = np.flatnonzero(temp_flag)
-    #temp_ind1 = np.intersect1d(temp_ind1, temp_ind2)
-    
-    
-    '''excluded = [ii for ii in range(len(temp_x1)) if ii not in temp_ind1]
+    valid_ind = np.where((np.isfinite(x_sort)==True) & (x_sort>0) & (x_sort < 1e13) & (temp_flag == 0))[0]
+
+
+    '''excluded = [ii for ii in range(len(x_sort)) if ii not in valid_ind]
     print("total =", len(excluded))
     #con1 = is not finite, con2 = less than (a) or equal to (b) zero, con3 = greater than (a) or equal to (b) 1e13
-    con1 = np.where(np.isfinite(temp_x1)==False)[0]
-    con2a = np.where(temp_x1 < 0)[0]
-    con2b = np.where(temp_x1 == 0)[0]
-    con3a = np.where(temp_x1 > 1e13)[0]
-    con3b = np.where(temp_x1 == 1e13)[0]
+    con1 = np.where(np.isfinite(x_sort)==False)[0]
+    con2a = np.where(x_sort < 0)[0]
+    con2b = np.where(x_sort == 0)[0]
+    con3a = np.where(x_sort > 1e13)[0]
+    con3b = np.where(x_sort == 1e13)[0]
     print("condition 1 =", len(con1))
     print("condition 2a =", len(con2a))
     print("condition 2b =", len(con2b))
     print("condition 3a =", len(con3a))
     print("condition 3b =", len(con3b))''' 
      
-    x = np.log10(temp_x1[temp_ind1])
-    ind = temp_ind[temp_ind1]
+    x = np.log10(x_sort[valid_ind])
+    ind = ind_sort[valid_ind]
     y = range(len(x))
+
     
     start = 0
     bin_start = x[start]
     bin_edge = []
-    bin_size = []
+    bin_redge = []
     distribution = []
     flux = []
     wavelength = []
-    count = 0
-    
+    mass_avg = []
+    bin_ID = []
     N = []
+    count = 0
+
+            
     while (bin_start < x[-1]):
+        if adaptive == True:
+            bin_pts = bin_pts_input[count]
+        else:
+            bin_pts = bin_pts_input
+            
         stop = start + bin_pts
         if ((stop + bin_pts) > len(x)):
             stop = len(x) - 1
@@ -114,18 +136,30 @@ def binning(temp_x, objno, bin_pts, interp_file, mname = '', bin_array_file = ''
         dist = len(y[start:stop])
         distribution.append(dist)
         bin_edge.append(bin_start)
-        bin_size.append((bin_stop - bin_start))
         if filename != False:
-            #print(start, stop, ind[start], ind[stop])
-            #if start == 0: print(np.log10(temp_x[ind[start:stop]]))
             _, flx, wave = stack_spectra(filename, mname, indices = ind[start:stop])
             flux.append(flx)
             wavelength.append(wave)
         N.append(len(ind[start:stop]))
+        mass_avg.append(np.mean(x[start:stop]))
+        bin_ID.append(count)
         if bin_array_file != '':
-            np.savez(bin_array_file+'_'+str(count)+'.npz', indices=ind[start:stop])
+            if adaptive == False:
+                np.savez(bin_array_file + '_' + str(count) + '.npz', indices=ind[start:stop])
+            else:
+                np.savez(bin_array_file + bin_pts_fname + '_' + str(count) + '.npz', indices=ind[start:stop])
+        
         start, bin_start = stop, bin_stop
+        bin_redge.append(bin_stop)
     
+    if adaptive == False:
+        out_ascii = path + str(bin_pts) + '_massbin.tbl' 
+    else:
+        out_ascii = path + bin_pts_fname + '_massbin.tbl'
+    n = ('ID', 'mass_min', 'mass_max', 'mass_avg', 'Number of Galaxies')
+    table_stack = Table([bin_ID, bin_edge, bin_redge, mass_avg, N], names = n)
+    ascii.write(table_stack, out_ascii, format = 'fixed_width_two_line', overwrite = True)
+        
     if (spectra_plot == True):
         for i in range(count):
             plt.subplot(np.ceil(count/2.0), 2, i+1)
@@ -138,11 +172,10 @@ def binning(temp_x, objno, bin_pts, interp_file, mname = '', bin_array_file = ''
             plt.annotate('N = '+str(N[i]), [0.05,0.95], xycoords='axes fraction',
                          ha='left', va='top')
     else:
-        plt.bar(bin_edge, distribution, align = 'edge', width = bin_size)
+        plt.bar(bin_edge, distribution, align = 'edge', width = bin_redge)
         
-        
+    
     return distribution, bin_edge, flux
-
 
 
 
