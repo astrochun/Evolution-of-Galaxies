7a4c8912f9cd97558342b68372201886615c20e8
Caroline McCormick <cmccormick1@email.arizona.edu>
2019-10-18 01:14:42 -0700

    Combined make_valid_table() and invalid_limit() into one function and implemented 5007 cut

--- valid_table.py
+++ valid_table.py
@@ -3,7 +3,7 @@ import numpy as np
 from astropy.table import Table, Column
 from getpass import getuser
 
-
+'''
 if getuser() == 'carol':
     path = "C:\\Users\\carol\\Google Drive\\MZEvolve\\"
     path2 = path + "massbin\\"
@@ -15,6 +15,7 @@ bin_pts_input = [75, 112, 113, 300, 600, 1444, 1444]
 str_bin_pts_input = [str(val) for val in bin_pts_input]
 bin_pts_fname = "_".join(str_bin_pts_input)
 bin_pts_fname = 'hbeta_revised_' + bin_pts_fname
+#bin_pts_fname = 'revised_75_112_113_300_600_1444_1444'
 
 N_in_bin = bin_pts_fname
 
@@ -23,31 +24,23 @@ if mark_nondet:
     updated = '_updated'
 else:
     updated = ''
+ '''
+   
 
-
-#revisit
-'''def set_luminosity():
-    #binning_file = np.load(path2 + 'mass_bin_' + bin_pts_fname + '.npz')
-    #lum = binning_file['lum']
-    em_table = asc.read(path2 + N_in_bin + '_massbin_emission_lines.tbl')
-    O_5007_rms = em_table['OIII_5007_RMS'].data
-    
-    invalid_lum_idx = np.where(lum == -1)[0]
-    updated_lum = lum
-    updated_lum[invalid_lum_idx] = 3 * O_5007_rms[invalid_lum_idx]'''
-    
-
-
-def invalid_limit():
-    em_table = asc.read(path2 + N_in_bin + '_massbin_emission_lines.tbl')
-    valid_table = asc.read(path2 + N_in_bin + '_massbin_validation.tbl')
+'''
+def invalid_limit(fitspath, bin_pts_fname):
+    em_table = asc.read(fitspath + bin_pts_fname + '_massbin_emission_lines.tbl')
+    valid_table = asc.read(fitspath + bin_pts_fname + '_massbin_validation.tbl')
     
     O_4363_flux = em_table['OIII_4363_Flux_Observed'].data
     O_4363_SN = em_table['OIII_4363_S/N'].data
+    O_5007_SN = em_table['OIII_5007_S/N'].data
     HGamma_rms = em_table['HGAMMA_RMS'].data
     valid_O_4363 = valid_table['Valid_OIII_4363'].data
     
-    invalid_stacks_idx = np.where((O_4363_SN < 3) | (valid_O_4363 == 0))[0]  
+    #correct?
+    invalid_stacks_idx = np.where((O_4363_SN < 3) | (valid_O_4363 == 0))[0] 
+    #implement 5007 cut?
     detections = np.ones(len(valid_O_4363))
     detections[invalid_stacks_idx] = 0
     
@@ -66,42 +59,71 @@ def invalid_limit():
     detection_col = Column(detections, name = 'Detection')
     updated_em_table.add_column(detection_col)
     asc.write(updated_em_table, out_ascii, format = 'fixed_width_two_line', overwrite = True)
+ '''
+   
     
 
 
-def make_validation_table():
-    massbin_table = asc.read(path2 + N_in_bin + '_massbin.tbl', format = 'fixed_width_two_line')
+def make_validation_table(fitspath, bin_pts_fname):
+    massbin_table = asc.read(fitspath + bin_pts_fname + '_massbin.tbl', format = 'fixed_width_two_line')
+    em_table = asc.read(fitspath + bin_pts_fname + '_massbin_emission_lines.tbl')
     ID = massbin_table['ID'].data
     mass_min = massbin_table['mass_min'].data
     mass_max = massbin_table['mass_max'].data
     mass_avg = massbin_table['mass_avg'].data
     N = massbin_table['Number of Galaxies'].data
-    #one means detection, zero means non-detection
-    valid_OIII_4363 = np.ones(len(ID))
     
-    if N_in_bin == '800':
-        valid_OIII_4363[1] = 0
-        valid_OIII_4363[4] = 0
+    O_4363_flux = em_table['OIII_4363_Flux_Observed'].data
+    O_4363_SN = em_table['OIII_4363_S/N'].data
+    O_5007_SN = em_table['OIII_5007_S/N'].data
+    HGamma_rms = em_table['HGAMMA_RMS'].data
+    
+    detections = np.zeros(len(ID))            #one means detection, zero means non-detection
+    
+    ##old method
+    '''
+    if bin_pts_fname == '800':
+        detections[1] = 0
+        detections[4] = 0
         
-    if N_in_bin == '75_112_113_300_600_1444_1444':
-        valid_OIII_4363[1] = 0
-        valid_OIII_4363[5] = 0
-        valid_OIII_4363[6] = 0   
+    if bin_pts_fname == '75_112_113_300_600_1444_1444':
+        detections[1] = 0
+        detections[5] = 0
+        detections[6] = 0   
         
-    if N_in_bin == 'revised_75_112_113_300_600_1444_1444':
-        valid_OIII_4363[3] = 0
-        valid_OIII_4363[5] = 0
-        valid_OIII_4363[6] = 0
+    if bin_pts_fname == 'revised_75_112_113_300_600_1444_1444':
+        detections[3] = 0
+        detections[5] = 0
+        detections[6] = 0
         
-    if N_in_bin == 'hbeta_revised_75_112_113_300_600_1444_1444':
-        valid_OIII_4363[2] = 0
-        valid_OIII_4363[8] = 0
-        valid_OIII_4363[10] = 0
-        valid_OIII_4363[12] = 0       
+    if bin_pts_fname == 'hbeta_revised_75_112_113_300_600_1444_1444':
+        detections[2] = 0
+        detections[8] = 0
+        detections[10] = 0
+        detections[12] = 0  
+    '''
+    ##
         
     
-    out_ascii = path2 + N_in_bin + '_massbin_validation.tbl'
-    n = ('ID', 'mass_min', 'mass_max', 'mass_avg', 'Number of Galaxies','Valid_OIII_4363')
-    valid_table = Table([ID, mass_min, mass_max, mass_avg, N, valid_OIII_4363], names = n)
+    valid_stacks_idx = np.where((O_4363_SN >= 3) & (O_5007_SN >= 100))[0] 
+    detections[valid_stacks_idx] = 1
+    invalid_stacks_idx = np.where(detections != 1)[0]
+    
+    updated_O_4363_flux = O_4363_flux
+    updated_O_4363_flux[invalid_stacks_idx] = 3 * HGamma_rms[invalid_stacks_idx]
+    
+    #####
+    
+    out_ascii = fitspath + bin_pts_fname + '_updated_emission_lines.tbl'
+    updated_em_table = Table(em_table)
+    updated_em_table['OIII_4363_Flux_Observed'] = updated_O_4363_flux
+    detection_col = Column(detections, name = 'Detection')
+    updated_em_table.add_column(detection_col)
+    asc.write(updated_em_table, out_ascii, format = 'fixed_width_two_line', overwrite = True)
+        
+    
+    out_ascii = fitspath + bin_pts_fname + '_massbin_validation.tbl'
+    n = ('ID', 'mass_min', 'mass_max', 'mass_avg', 'Number of Galaxies', 'Detection')
+    valid_table = Table([ID, mass_min, mass_max, mass_avg, N, detections], names = n)
     asc.write(valid_table, out_ascii, format = 'fixed_width_two_line', overwrite = True)
     
