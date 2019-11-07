b00485ebabe4720fa44a33644559877672181233
Caroline McCormick <cmccormick1@email.arizona.edu>
2019-11-07 13:50:55 -0700

    Moved a, b, and c to R_temp_calcul.py and added np.zero arrays of EBV, k_4363, and k_5007.

--- general.py
+++ general.py
@@ -13,6 +13,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.cosmology import FlatLambdaCDM
 import astropy.units as u
+from chun_codes.cardelli import *
 from Evolution_of_Galaxies import library, emission_line_fit, R_temp_calcul, valid_table, plots #, indiv_gals
 #from Zcalbase_gal import error_prop
 
@@ -122,16 +123,17 @@ def run_bin_analysis():
     valid_table.make_validation_table(fitspath, bin_pts_fname)
     
     
+    #Run dust attenuation
+    #change later once function is implemented and get actual values
+    EBV = np.zeros(len(line_name))
+    k_4363 = np.zeros(len(line_name))
+    k_5007 = np.zeros(len(line_name))
     
-    #Run R, Te, and Metallicity calculations    
-    a = 13205
-    b = 0.92506
-    c = 0.98062
     
+    #Run R, Te, and Metallicity calculations        
     em_file = fitspath + bin_pts_fname + '_emission_lines.tbl'
     outfile = fitspath + bin_pts_fname + '_derived_properties_metallicity'
-    R_temp_calcul.run_function(em_file, outfile, a, b, c)
-    
+    R_temp_calcul.run_function(em_file, outfile, EBV, k_4363, k_5007)
     
     
     #Run plots
@@ -140,31 +142,26 @@ def run_bin_analysis():
     
     '''
     #Run error propagation
-    em_file = fitspath + bin_pts_fname + '_updated_emission_lines.tbl'
-    error_prop.error_prop_chuncodes(fitspath, em_file)
-    error_prop.plotting_errors()
+    error_prop.error_prop_chuncodes(fitspath + bin_pts_fname, em_file, outfile + '.tbl')
     '''
     
 
     
     
-'''
-Adapt
-    
-
-from chun_codes.cardelli import *
-def dust_attenuation(fitspath, combine_ascii):
-    line_name = ['OII_3727','NeIII','HeI','3967', 'HDELTA', 'Hgamma', 'OIII_4363', 'HBETA', 'OIII_4958','OIII_5007']
+'''    
+#combine_ascii is emission_line.tbl for me
+def dust_attenuation(fitspath, bin_pts_fname, combine_ascii):
+    line_name = ['OII_3727', 'HDELTA', 'HGAMMA', 'OIII_4363', 'HBETA', 'OIII_4958','OIII_5007']
     
     combine_asc = asc.read(combine_ascii)
     ini_con = 0.468
     ID = combine_asc['ID']
-    HBeta= combine_asc['HBETA_Flux_Observed'].data
-    HGamma= combine_asc['Hgamma_Flux_Observed'].data
+    HBeta = combine_asc['HBETA_Flux_Observed'].data
+    HGamma = combine_asc['HGAMMA_Flux_Observed'].data
     
     lam0_OII = combine_asc['OII_3727_X_bar'].data
     lam0_HDELTA = combine_asc['HDELTA_X_bar'].data
-    lam0_Hgamma = combine_asc['Hgamma_X_bar'].data
+    lam0_Hgamma = combine_asc['HGAMMA_X_bar'].data
     lam0_HBETA = combine_asc['HBETA_X_bar'].data
     lam0_4363 = combine_asc['OIII_4363_X_bar'].data
     lam0_4958 = combine_asc['OIII_4958_X_bar'].data
@@ -184,7 +181,7 @@ def dust_attenuation(fitspath, combine_ascii):
     for nn in range(len(HGamma)):
         if EBV[nn] <= 0: EBV[nn] = 0
     
-    print EBV
+    #print EBV
     A_3727 = EBV*k_3727
     A_HDELTA = EBV*k_HDELTA
     A_Hgamma = EBV*k_Hgamma
@@ -192,14 +189,13 @@ def dust_attenuation(fitspath, combine_ascii):
     A_4363 = EBV*k_4363
     A_4958 = EBV*k_4958
     A_5007 = EBV*k_5007
-    print "A_3727:", A_3727
+    #print "A_3727:", A_3727
 
     out_ascii = fitspath+'/dust_attentuation_values.tbl'
     #if not exists(out_ascii_single):
     n2= ('ID','k_3727', 'k_HDELTA', 'k_Hgamma', 'k_HBETA', 'k_4363', 'k_4958', 'k_5007', 'E(B-V)')
     tab1 = Table([ID,k_3727, k_HDELTA, k_Hgamma, k_HBETA , k_4363, k_4958, k_5007, EBV], names=n2)
     asc.write(tab1, out_ascii, format='fixed_width_two_line')
-    
     
     
 def call_cardelli(lam0): #, extrapolate=False):
