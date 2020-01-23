Unimplemented Metallicity_Stack_Commons to fix missing factor of 2 in denominator exp in gauss() function and obtain control results. Issue #6 fixed.

emission_line_fit.py
@@ -11,12 +11,13 @@ from astropy.table import Table, hstack
 from pylab import subplots_adjust
 from scipy.optimize import curve_fit 
 from astropy.convolution import Box1DKernel, convolve
-from Metallicity_Stack_Commons.fitting import movingaverage_box1D, gauss, double_gauss, oxy2_gauss, rms_func, con1
-from Metallicity_Stack_Commons import scalefact
+#from Metallicity_Stack_Commons.fitting import movingaverage_box1D, gauss, double_gauss, oxy2_gauss, rms_func, con1
+#from Metallicity_Stack_Commons import scalefact
 
+scalefact = 1e-17
 
 ##Old
-'''
+
 def movingaverage_box1D(values, width, boundary = 'fill', fill_value = 0.0):
     box_kernel = Box1DKernel(width)
     smooth = convolve(values, box_kernel, boundary = boundary, fill_value = fill_value)
@@ -24,7 +25,7 @@ def movingaverage_box1D(values, width, boundary = 'fill', fill_value = 0.0):
 
 
 def gauss(x, xbar, s, a, c):
-    return a * np.exp(-(x - xbar)**2 / s**2) + c
+    return a * np.exp(-(x - xbar)**2 / (2 * s**2)) + c
 
 
 def double_gauss(x, xbar, s1, a1, c, s2, a2): 
@@ -34,7 +35,7 @@ def double_gauss(x, xbar, s1, a1, c, s2, a2):
 def oxy2_gauss(x, xbar, s1, a1, c, s2, a2):
     con1 = 3728.91/3726.16
     return a1 * np.exp(-(x - xbar)**2 / (2 * s1**2)) + c + a2 * np.exp(-(x - (xbar * con1))**2 / (2 * s2**2))
- '''
+
 ##
 
 def get_gaussian_fit(working_wave, x0, y0, y_norm, x_idx, x_idx_mask, line_type, s2):
@@ -75,9 +76,9 @@ def get_gaussian_fit(working_wave, x0, y0, y_norm, x_idx, x_idx_mask, line_type,
         return None, med0, max0
 
 ##Old
-'''
+
 #calculating rms
-def rms_func(wave, dispersion, lambda_in, y0, sigma_array, scalefact, mask_flag):
+def rms_func(wave, dispersion, lambda_in, y0, sigma_array, mask_flag):
 
     x_idx = np.where((np.abs(wave - lambda_in) <= 100) & (mask_flag == 0))[0]
 
@@ -89,7 +90,7 @@ def rms_func(wave, dispersion, lambda_in, y0, sigma_array, scalefact, mask_flag)
     RMS_pix = sigma * dispersion / scalefact
    
     return ini_sig / scalefact, RMS_pix
-'''
+
 ##
 
 
@@ -209,7 +210,7 @@ def zoom_gauss_plot(pdf_pages, N, wave, Spect_1D, dispersion, s2, lambda0, worki
                 y_norm_diff = y_norm[x_sigsnip] - neg0[x_sigsnip]
 
             if line_type == 'Oxy2':
-                #con1 = 3728.91/3726.16
+                con1 = 3728.91/3726.16  ##
                 x_sigsnip = np.where(((x0 - working_wave) / o1[1] >= -2.5) & ((x0 - working_wave * con1) / o1[4] <= 2.5))[0]
                 gauss0 = oxy2_gauss(x0, *o1)
             