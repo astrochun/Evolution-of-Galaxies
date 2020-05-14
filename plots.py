6a8a1f92625b0e63b0d7d878f4a47cae779db50a
Caroline McCormick <cmccormick1@email.arizona.edu>
2020-05-14 14:25:08 -0700

    (Issue #25) Replaced the curve fitting code in indiv_derived_props_plots() function with a call to the new curve fitting function.

--- plots.py
+++ plots.py
@@ -243,23 +243,7 @@ def indiv_derived_props_plots(fitspath, restrict_MTO = False, revised = False, e
         ax11.errorbar(bin_logM[bin_detect], bin_metal[bin_detect], yerr = metal_lowhigh_err, fmt = '.')
     
     
-    ##Curve fit     
-    fail = False
-    if restrict_MTO == False:
-        p0 = [8.798, 8.901, 0.640]
-        para_bounds = ((8.0, 8.0, 0.0), (9.0, 9.5, 1.0))
-        fit = mass_metal_fit
-    else:
-        p0 = [8.798, 0.640]
-        para_bounds = ((8.0, 0.0), (9.0, 1.0))
-        fit = mass_metal_fit2
-        
-    try:
-        o11, o21 = curve_fit(fit, bin_logM[bin_detect], bin_metal[bin_detect], p0 = p0, bounds = para_bounds)
-        print(o11)
-    except ValueError:
-        print('Failed curve fitting')
-        fail = True
+    o11, o21, fail = curve_fitting(bin_logM[bin_detect], bin_metal[bin_detect], restrict_MTO = False)
         
     if not fail:
         if restrict_MTO == False:
