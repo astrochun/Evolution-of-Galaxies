f7dd93008fd35ff3967911164af1c01b36c5460c
Caroline McCormick <cmccormick1@email.arizona.edu>
2020-05-18 11:30:45 -0700

    (Issue #25) Added a new function called extract_error_bars that puts the error bar arrays in the correct order for plotting.

--- Plotting/relation_fitting.py
+++ Plotting/relation_fitting.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.optimize import curve_fit 
+from astropy.io import ascii as asc
+from Metallicity_Stack_Commons.column_names import npz_filename_dict, filename_dict, temp_metal_names0, bin_names0
 
 
 def curve_fitting(x_array, y_array, restrict_MTO = False):
@@ -47,6 +49,24 @@ def mass_metal_fit_constMTO(mass, a, g):
     
     return a - np.log10(1 + ((10**8.901)/(10**mass))**g) 
 
+
+
+def extract_error_bars(fitspath):
+    der_prop_err = np.load(fitspath + npz_filename_dict['der_prop_errors'])
+    der_prop_file = fitspath + filename_dict['bin_derived_prop_rev']
+    valid_tbl = asc.read(fitspath + filename_dict['bin_valid_rev'], format = 'fixed_width_two_line')
+        
+    Te_err = der_prop_err['T_e_lowhigh_error']        
+    metal_err = der_prop_err['12+log(O/H)_lowhigh_error']
+    T_e = der_prop_file[temp_metal_names0[0]].data
+    detect = np.where(valid_tbl[bin_names0[2]].data == 1.0)[0]
+        
+    Te_low_err = -1*np.log10(1 - Te_err[:,0]/T_e[detect])
+    Te_high_err = np.log10(1 + Te_err[:,1]/T_e[detect])
+    
+    err_dict = {'T_e_lowhigh_error': [Te_low_err, Te_high_err], '12+log(O/H)_lowhigh_error': [metal_err[:,0], metal_err[:,1]]}
+    
+    return err_dict
 
 
 
