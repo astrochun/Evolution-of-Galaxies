208e73ad132c145e4e79f0a0f57ed21881db3805
Caroline McCormick <cmccormick1@email.arizona.edu>
2020-07-09 18:02:59 -0700

    (Issue #38) Utilized dictionaries in indiv_bin_info_table function to store bin_info.tbl data columns read in and the data to be saved to individual_bin_info.tbl

--- Analysis/indiv_gals.py
+++ Analysis/indiv_gals.py
@@ -1,14 +1,13 @@
 import numpy as np
 from astropy.io import fits
 from astropy.io import ascii as asc
-from astropy.table import Table
 from Metallicity_Stack_Commons.column_names import filename_dict, bin_names0, bin_mzevolve_names0, indv_names0, line_fit_suffix_add
 from Metallicity_Stack_Commons import line_name, line_type
     
 
 
 
-def indiv_bin_info_table(fitspath, line_file, LHb_bin = False, use_revised = False):
+def indiv_bin_info_table(fitspath, line_file, use_revised = False):
     '''
     Purpose:
         This function creates a table containing bin information such as bin ID numbers, minimum, 
@@ -18,7 +17,6 @@ def indiv_bin_info_table(fitspath, line_file, LHb_bin = False, use_revised = Fal
     Params:
         fitspath --> file path where bin files are located and where the output file will be located
         line_file --> a fits table containing individual galaxy ID numbers
-        LHb_bin (OPTIONAL) --> boolean to determine which binning type was used (default is False)
         use_revised (OPTIONAL) --> boolean to use the revised validation table or not (default is False)
         
     Returns:
@@ -28,57 +26,54 @@ def indiv_bin_info_table(fitspath, line_file, LHb_bin = False, use_revised = Fal
         'individual_bin_info.tbl'
     '''
     
-    if LHb_bin == True:
-        bin_type = 'logLHb'
-    else:
-        bin_type = 'logM'
-    
     bin_npz = np.load(fitspath + filename_dict['bin_info'].replace('.tbl', '.npz'))
-    bin_tbl = asc.read(fitspath + filename_dict['bin_info'])
+    tbl = asc.read(fitspath + filename_dict['bin_info'])
     hdu = fits.open(line_file)
     if use_revised == True:
         valid_tbl = asc.read(fitspath + filename_dict['bin_valid_rev'])
     else:
         valid_tbl = asc.read(fitspath + filename_dict['bin_valid'])
+        
+    #Convert bin info table into a dictionary
+    cols = tbl.colnames
+    bin_dict = {}
+    for col in cols:
+        if col not in bin_dict:
+            bin_dict[col] = tbl[col].data
     
     
     line_table = hdu[1].data
     objno = line_table['OBJNO']
-    bin_ID = bin_tbl[bin_names0[0]].data
-    bin_min = bin_tbl[bin_type + '_min'].data
-    bin_max = bin_tbl[bin_type + '_max'].data
-    bin_avg = bin_tbl[bin_type + '_avg'].data
-    bin_median = bin_tbl[bin_type + '_median'].data
     bin_idx = bin_npz['bin_ind']     #valid mass bin indices relative to unsorted data table 
     detect = valid_tbl[bin_names0[2]].data                   
     
     
-    #Get all bin valid values in 1D array
+    #Get all bin valid values in dictionary
+    bin_cols = [bin_names0[0], bin_names0[2], indv_names0[0]] + bin_mzevolve_names0
+    indiv_dict = {}
+    for name in bin_cols:
+        indiv_dict[name] = []
+    
     valid_idx = []
-    bin_ID_array = []
-    bin_min_array = []
-    bin_max_array = []
-    bin_avg_array = []
-    bin_median_array = []
-    detect_array = []
     for ii in range(len(bin_idx)):
         valid_idx += list(bin_idx[ii])
         for jj in range(len(bin_idx[ii])):
-            bin_ID_array.append(bin_ID[ii]) 
-            bin_min_array.append(bin_min[ii])
-            bin_max_array.append(bin_max[ii])
-            bin_avg_array.append(bin_avg[ii])
-            bin_median_array.append(bin_median[ii]) 
-            detect_array.append(detect[ii])            
-    idx_array = np.array(valid_idx)                     
+            indiv_dict['bin_ID'] += [bin_dict['bin_ID'][ii]] 
+            indiv_dict['Detection'] += [detect[ii]]
+            indiv_dict['logM_min'] += [bin_dict['logM_min'][ii]]
+            indiv_dict['logM_max'] += [bin_dict['logM_max'][ii]]
+            indiv_dict['logM_avg'] += [bin_dict['logM_avg'][ii]]
+            indiv_dict['logM_median'] += [bin_dict['logM_median'][ii]]
+            indiv_dict['logLHb_min'] += [bin_dict['logLHb_min'][ii]]
+            indiv_dict['logLHb_max'] += [bin_dict['logLHb_max'][ii]]
+            indiv_dict['logLHb_avg'] += [bin_dict['logLHb_avg'][ii]]
+            indiv_dict['logLHb_median'] += [bin_dict['logLHb_median'][ii]]
+    idx_array = np.array(valid_idx)
+    indiv_dict['ID'] = objno[idx_array]          
 
     
-    out_ascii = fitspath + filename_dict['indv_bin_info']  
-    bin_cols = [bin_names0[0], bin_names0[2], indv_names0[0]]
-    bin_cols += [col for col in bin_mzevolve_names0 if col.startswith(bin_type)]
-    indiv_bin_info_tbl = Table([bin_ID_array, detect_array, objno[idx_array], bin_min_array, bin_max_array, bin_avg_array,
-                                bin_median_array], names = tuple(bin_cols))
-    asc.write(indiv_bin_info_tbl, out_ascii, format = 'fixed_width_two_line', overwrite = True)
+    out_ascii = fitspath + filename_dict['indv_bin_info']
+    asc.write(indiv_dict, names = tuple(bin_cols), output = out_ascii, format = 'fixed_width_two_line', overwrite = True)
     
     
     hdu.close()
@@ -140,5 +135,4 @@ def indiv_em_table(fitspath, line_file):
         data[cols[jj]] = line_table[line_tbl_col[jj]]
 
     out_ascii = fitspath + filename_dict['indv_prop']
-    indiv_props_tbl = Table(data, names = tuple([indv_names0[0]] + indv_names0[3:5] + cols))
-    asc.write(indiv_props_tbl, out_ascii, format = 'fixed_width_two_line', overwrite = True) 
+    asc.write(data, names = tuple([indv_names0[0]] + indv_names0[3:5] + cols), output = out_ascii, format = 'fixed_width_two_line', overwrite = True) 
