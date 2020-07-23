ed0ef6508ab44d1df44806561b1087a601b4f2e5
Caroline McCormick <cmccormick1@email.arizona.edu>
2020-07-23 14:07:33 -0700

    (PR #41) Minor formatting changes.

--- Plotting/composite_plots.py
+++ Plotting/composite_plots.py
@@ -30,7 +30,7 @@ def bin_derived_props_plots(fitspath, hbeta_bin = False, err_bars = False, revis
         pdf_pages --> a pdf containing all of the plots for the stacked spectra on separate pages.
     '''
     
-    #Read in data tables
+    # Read in data tables
     if revised:
         pdf_pages = PdfPages(fitspath + filename_dict['bin_derived_prop_rev'].replace('.tbl', '.pdf'))
         metal_table = asc.read(fitspath + filename_dict['bin_derived_prop_rev'])
@@ -41,7 +41,7 @@ def bin_derived_props_plots(fitspath, hbeta_bin = False, err_bars = False, revis
         valid_table = asc.read(fitspath + filename_dict['bin_valid'])
     bin_table = asc.read(fitspath + filename_dict['bin_info'])
     
-    #Read in composite values
+    # Read in composite values
     com_O_log = metal_table[temp_metal_names0[1]].data
     T_e = metal_table[temp_metal_names0[0]].data
     logR23 = metal_table[bin_ratios0[0]].data
@@ -57,14 +57,14 @@ def bin_derived_props_plots(fitspath, hbeta_bin = False, err_bars = False, revis
     detect = np.where(detection == 1)[0]
     non_detect = np.where(detection == 0.5)[0]
     
-    if err_bars == True:
+    if err_bars:
         err_dict = extract_error_bars(fitspath)
     
-    #Line Ratios vs Mass
+    # Line Ratios vs Mass
     fig1, ax1 = plt.subplots(2, 3, sharex = True)
     
     for ii in range(len(detect)):
-        if hbeta_bin == True:
+        if hbeta_bin:
             if detect[ii] % 2 == 0:
                 pt_color = 'cyan'
             else:
@@ -78,26 +78,34 @@ def bin_derived_props_plots(fitspath, hbeta_bin = False, err_bars = False, revis
         ax1[1, 0].scatter(logM_avg[detect[ii]], log_threeBeta[detect[ii]], marker = '.', color = pt_color)
         ax1[1, 1].scatter(logM_avg[detect[ii]], logR[detect[ii]], marker = '.', color = pt_color)
     for ii in range(len(non_detect)):
-        if hbeta_bin == True:
+        if hbeta_bin:
             if non_detect[ii] % 2 == 0:
                 pt_color = 'pink'
             else:
                 pt_color = 'red'
-            ax1[1, 2].scatter(logM_avg[non_detect[ii]], logLHb_avg[non_detect[ii]], marker = '^', color = pt_color)
+            ax1[1, 2].scatter(logM_avg[non_detect[ii]], logLHb_avg[non_detect[ii]], marker = '^', 
+                              color = pt_color)
         else:
             pt_color = 'orange'
         ax1[0, 0].scatter(logM_avg[non_detect[ii]], logR23[non_detect[ii]], marker = '^', color = pt_color)
         ax1[0, 1].scatter(logM_avg[non_detect[ii]], logO32[non_detect[ii]], marker = '^', color = pt_color)
-        ax1[0, 2].scatter(logM_avg[non_detect[ii]], log_twoBeta[non_detect[ii]], marker = '^', color = pt_color)
-        ax1[1, 0].scatter(logM_avg[non_detect[ii]], log_threeBeta[non_detect[ii]], marker = '^', color = pt_color)
+        ax1[0, 2].scatter(logM_avg[non_detect[ii]], log_twoBeta[non_detect[ii]], marker = '^', 
+                          color = pt_color)
+        ax1[1, 0].scatter(logM_avg[non_detect[ii]], log_threeBeta[non_detect[ii]], marker = '^', 
+                          color = pt_color)
         ax1[1, 1].scatter(logM_avg[non_detect[ii]], logR[non_detect[ii]], marker = '^', color = pt_color)
     
-    ax1[0, 0].annotate('$R_{23}$', [0.05, 0.97], xycoords = 'axes fraction', va = 'top', ha = 'left', fontsize = '7')
-    ax1[0, 1].annotate('$O_{32}$', [0.05, 0.97], xycoords = 'axes fraction', va = 'top', ha = 'left', fontsize = '7')
-    ax1[0, 2].annotate('[OII]/H$\\beta$', [0.05, 0.97], xycoords = 'axes fraction', va = 'top', ha = 'left', fontsize = '7')
-    ax1[1, 0].annotate('[OIII]/H$\\beta$', [0.05, 0.97], xycoords = 'axes fraction', va = 'top', ha = 'left', fontsize = '7')
+    ax1[0, 0].annotate('$R_{23}$', [0.05, 0.97], xycoords = 'axes fraction', va = 'top', ha = 'left', 
+                       fontsize = '7')
+    ax1[0, 1].annotate('$O_{32}$', [0.05, 0.97], xycoords = 'axes fraction', va = 'top', ha = 'left', 
+                       fontsize = '7')
+    ax1[0, 2].annotate('[OII]/H$\\beta$', [0.05, 0.97], xycoords = 'axes fraction', va = 'top', ha = 'left', 
+                       fontsize = '7')
+    ax1[1, 0].annotate('[OIII]/H$\\beta$', [0.05, 0.97], xycoords = 'axes fraction', va = 'top', ha = 'left', 
+                       fontsize = '7')
     ax1[1, 1].annotate('R', [0.05, 0.97], xycoords = 'axes fraction', va = 'top', ha = 'left', fontsize = '7')
-    ax1[1, 2].annotate('H$\\beta$ Luminosity', [0.05, 0.97], xycoords = 'axes fraction', va = 'top', ha = 'left', fontsize = '7')  
+    ax1[1, 2].annotate('H$\\beta$ Luminosity', [0.05, 0.97], xycoords = 'axes fraction', va = 'top', 
+                       ha = 'left', fontsize = '7')  
     
     for ii in range(0, 3):    
         ax1[1, ii].set_xlabel('log($\\frac{M_\star}{M_{\odot}}$)')     
@@ -108,11 +116,11 @@ def bin_derived_props_plots(fitspath, hbeta_bin = False, err_bars = False, revis
     pdf_pages.savefig()  
     
     
-    #log(R23) vs log(Temperature)
+    # log(R23) vs log(Temperature)
     fig2, ax2 = plt.subplots()
     ax2.scatter(np.log10(T_e[detect]), logR23[detect], marker = '.')
     ax2.scatter(np.log10(T_e[non_detect]), logR23[non_detect], marker = '<')
-    if err_bars == True:
+    if err_bars:
         ax2.errorbar(np.log10(T_e[detect]), logR23[detect], xerr = err_dict['T_e_lowhigh_error'], fmt = '.')
     ax2.set_xlabel('log($T_e$) (K)')
     ax2.set_ylabel('log($R_{23}$)')
@@ -120,11 +128,11 @@ def bin_derived_props_plots(fitspath, hbeta_bin = False, err_bars = False, revis
     pdf_pages.savefig()
     
      
-    #log(O32) vs log(Temperature)
+    # log(O32) vs log(Temperature)
     fig3, ax3 = plt.subplots()
     ax3.scatter(np.log10(T_e[detect]), logO32[detect], marker = '.')
     ax3.scatter(np.log10(T_e[non_detect]), logO32[non_detect], marker = '<')
-    if err_bars == True:
+    if err_bars:
         ax3.errorbar(np.log10(T_e[detect]), logO32[detect], xerr = err_dict['T_e_lowhigh_error'], fmt = '.')
     ax3.set_xlabel('log($T_e$) (K)')
     ax3.set_ylabel('log($O_{32}$)')
@@ -132,35 +140,37 @@ def bin_derived_props_plots(fitspath, hbeta_bin = False, err_bars = False, revis
     pdf_pages.savefig()
     
     
-    #log(Composite Metallicity) vs log(R23)
+    # log(Composite Metallicity) vs log(R23)
     fig4, ax4 = plt.subplots()
     ax4.scatter(logR23[detect], com_O_log[detect], marker = '.')
     ax4.scatter(logR23[non_detect], com_O_log[non_detect], marker = '^')
-    if err_bars == True:
-        ax4.errorbar(logR23[detect], com_O_log[detect], yerr = err_dict['12+log(O/H)_lowhigh_error'], fmt = '.')
+    if err_bars:
+        ax4.errorbar(logR23[detect], com_O_log[detect], yerr = err_dict['12+log(O/H)_lowhigh_error'], 
+                     fmt = '.')
     ax4.set_xlabel('log($R_{23}$)')
     ax4.set_ylabel('12+log(O/H) $T_e$')
     ax4.set_title('Composite Metallicity vs. $R_{23}$')
     pdf_pages.savefig()
     
     
-    #log(Composite Metallicity) vs log(O32)
+    # log(Composite Metallicity) vs log(O32)
     fig5, ax5 = plt.subplots()
     ax5.scatter(logO32[detect], com_O_log[detect], marker = '.')
     ax5.scatter(logO32[non_detect], com_O_log[non_detect], marker = '^')
-    if err_bars == True:
-        ax5.errorbar(logO32[detect], com_O_log[detect], yerr = err_dict['12+log(O/H)_lowhigh_error'], fmt = '.')
+    if err_bars:
+        ax5.errorbar(logO32[detect], com_O_log[detect], yerr = err_dict['12+log(O/H)_lowhigh_error'], 
+                     fmt = '.')
     ax5.set_xlabel('log($O_{32}$)')
     ax5.set_ylabel('12+log(O/H) $T_e$')
     ax5.set_title('Composite Metallicity vs. $O_{32}$')
     pdf_pages.savefig()
     
     
-    #log(Temperature) vs log(Avg Mass)
+    # log(Temperature) vs log(Avg Mass)
     fig6, ax6 = plt.subplots()
     ax6.scatter(logM_avg[detect], np.log10(T_e[detect]), marker = '.')
     ax6.scatter(logM_avg[non_detect], np.log10(T_e[non_detect]), marker = 'v')
-    if err_bars == True:
+    if err_bars:
         ax6.errorbar(logM_avg[detect], np.log10(T_e[detect]), yerr = err_dict['T_e_lowhigh_error'], fmt = '.')
     ax6.set_xlabel('log($\\frac{M_\star}{M_{\odot}}$)')
     ax6.set_ylabel('log($T_e$) (K)')
@@ -168,15 +178,17 @@ def bin_derived_props_plots(fitspath, hbeta_bin = False, err_bars = False, revis
     pdf_pages.savefig()
     
     
-    #log(Composite Metallicity) vs log(Avg Mass)
+    # log(Composite Metallicity) vs log(Avg Mass)
     fig7, ax7 = plt.subplots()
     ax7.scatter(logM_avg[detect], com_O_log[detect], marker = '.')
     ax7.scatter(logM_avg[non_detect], com_O_log[non_detect], marker = '^')
-    if err_bars == True:
-        ax7.errorbar(logM_avg[detect], com_O_log[detect], yerr = err_dict['12+log(O/H)_lowhigh_error'], fmt = '.')
+    if err_bars:
+        ax7.errorbar(logM_avg[detect], com_O_log[detect], yerr = err_dict['12+log(O/H)_lowhigh_error'], 
+                     fmt = '.')
     mass_range = np.arange(8.2, 9.9, 0.05)
     AM_relation = AM13(mass_range)
-    ax7.plot(mass_range, AM_relation, color='g', linestyle = '-', alpha = 0.5, label = 'Andrews & Martini (2013)')
+    ax7.plot(mass_range, AM_relation, color='g', linestyle = '-', alpha = 0.5, 
+             label = 'Andrews & Martini (2013)')
     ax7.set_xlabel('log($\\frac{M_\star}{M_{\odot}}$)')
     ax7.set_ylabel('12+log(O/H) $T_e$')
     ax7.set_title('Composite Metallicity vs. Avg Mass')
