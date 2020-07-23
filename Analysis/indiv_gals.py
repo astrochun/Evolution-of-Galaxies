3fd2afc2a4d5587750e7bfb1deb610832e265f30
Caroline McCormick <cmccormick1@email.arizona.edu>
2020-07-23 13:59:18 -0700

    (PR #41) Minor formatting changes, and in indiv_em_table(), I replaced the for loop to flatten the bin_ind array with np.hstack function.

--- Analysis/indiv_gals.py
+++ Analysis/indiv_gals.py
@@ -2,7 +2,8 @@ import numpy as np
 from astropy.io import fits
 from astropy.io import ascii as asc
 
-from Metallicity_Stack_Commons.column_names import filename_dict, bin_names0, bin_mzevolve_names0, indv_names0, line_fit_suffix_add
+from Metallicity_Stack_Commons.column_names import filename_dict, bin_names0, bin_mzevolve_names0
+from Metallicity_Stack_Commons.column_names import indv_names0, line_fit_suffix_add
 from Metallicity_Stack_Commons import line_name, line_type
 from Evolution_of_Galaxies.general import table_to_dict
     
@@ -28,7 +29,7 @@ def indiv_bin_info_table(fitspath, line_file, use_revised = False):
     
     bin_npz = np.load(fitspath + filename_dict['bin_info'].replace('.tbl', '.npz'))
     hdu = fits.open(line_file)
-    if use_revised == True:
+    if use_revised:
         valid_tbl = asc.read(fitspath + filename_dict['bin_valid_rev'])
     else:
         valid_tbl = asc.read(fitspath + filename_dict['bin_valid'])
@@ -42,8 +43,8 @@ def indiv_bin_info_table(fitspath, line_file, use_revised = False):
     detect = valid_tbl[bin_names0[2]].data  
     N_stack = valid_tbl[bin_names0[1]].data             
     
-    valid_idx = np.hstack(bin_idx)
     # Get all bin valid values in dictionary
+    valid_idx = np.hstack(bin_idx)
     bin_cols = [bin_names0[0], bin_names0[2], indv_names0[0]] + bin_mzevolve_names0
     indiv_dict = {}
     for name in bin_cols:
@@ -54,20 +55,21 @@ def indiv_bin_info_table(fitspath, line_file, use_revised = False):
         end = start + N_stack[ii]
         arr = np.arange(start, end)
         start = end
-        indiv_dict[bin_names0[0]][arr] = bin_dict[bin_names0[0]][ii]                     #bin_ID
-        indiv_dict[bin_names0[2]][arr] = detect[ii]                                      #Detection
-        indiv_dict[bin_mzevolve_names0[0]][arr] = bin_dict[bin_mzevolve_names0[0]][ii]   #logM_min
-        indiv_dict[bin_mzevolve_names0[1]][arr] = bin_dict[bin_mzevolve_names0[1]][ii]   #logM_max
-        indiv_dict[bin_mzevolve_names0[2]][arr] = bin_dict[bin_mzevolve_names0[2]][ii]   #logM_avg
-        indiv_dict[bin_mzevolve_names0[3]][arr] = bin_dict[bin_mzevolve_names0[3]][ii]   #logM_median
-        indiv_dict[bin_mzevolve_names0[4]][arr] = bin_dict[bin_mzevolve_names0[4]][ii]   #logLHb_min
-        indiv_dict[bin_mzevolve_names0[5]][arr] = bin_dict[bin_mzevolve_names0[5]][ii]   #logLHb_max
-        indiv_dict[bin_mzevolve_names0[6]][arr] = bin_dict[bin_mzevolve_names0[6]][ii]   #logLHb_avg
-        indiv_dict[bin_mzevolve_names0[7]][arr] = bin_dict[bin_mzevolve_names0[7]][ii]   #logLHb_median
-    indiv_dict[indv_names0[0]] = objno[valid_idx]                                        #ID     
+        indiv_dict[bin_names0[0]][arr] = bin_dict[bin_names0[0]][ii]                     # bin_ID
+        indiv_dict[bin_names0[2]][arr] = detect[ii]                                      # Detection
+        indiv_dict[bin_mzevolve_names0[0]][arr] = bin_dict[bin_mzevolve_names0[0]][ii]   # logM_min
+        indiv_dict[bin_mzevolve_names0[1]][arr] = bin_dict[bin_mzevolve_names0[1]][ii]   # logM_max
+        indiv_dict[bin_mzevolve_names0[2]][arr] = bin_dict[bin_mzevolve_names0[2]][ii]   # logM_avg
+        indiv_dict[bin_mzevolve_names0[3]][arr] = bin_dict[bin_mzevolve_names0[3]][ii]   # logM_median
+        indiv_dict[bin_mzevolve_names0[4]][arr] = bin_dict[bin_mzevolve_names0[4]][ii]   # logLHb_min
+        indiv_dict[bin_mzevolve_names0[5]][arr] = bin_dict[bin_mzevolve_names0[5]][ii]   # logLHb_max
+        indiv_dict[bin_mzevolve_names0[6]][arr] = bin_dict[bin_mzevolve_names0[6]][ii]   # logLHb_avg
+        indiv_dict[bin_mzevolve_names0[7]][arr] = bin_dict[bin_mzevolve_names0[7]][ii]   # logLHb_median
+    indiv_dict[indv_names0[0]] = objno[valid_idx]                                        # ID     
 
     out_ascii = fitspath + filename_dict['indv_bin_info']
-    asc.write(indiv_dict, names=tuple(bin_cols), output=out_ascii, format='fixed_width_two_line', overwrite=True)
+    asc.write(indiv_dict, names=tuple(bin_cols), output=out_ascii, format='fixed_width_two_line', 
+              overwrite=True)
     
     hdu.close()
   
@@ -89,17 +91,14 @@ def indiv_em_table(fitspath, line_file):
         'individual_properties.tbl'
     '''
     
-    #Read in individual line measurement file and bin information file
+    # Read in individual line measurement file and bin information file
     hdu = fits.open(line_file)
     bin_npz = np.load(fitspath + filename_dict['bin_info'].replace('.tbl', '.npz'))
     
-    #Get indices valid for binning
-    valid_idx = []
-    for ii in range(len(bin_npz['bin_ind'])):
-        valid_idx += list(bin_npz['bin_ind'][ii])
-    idx_array = np.array(valid_idx)
+    # Get indices valid for binning
+    idx_array = np.hstack(bin_npz['bin_ind'])
     
-    #Get column names from individual line measurement file
+    # Get column names from individual line measurement file
     line_tbl_names = ['_FLUX_MOD', '_FLUX_DATA', '_SNR', '_LAMBDA', '_PEAK', '_Y0', '_SIGMA', '_NOISE']
     line_tbl_prefixes = ['OIIB', 'HB', 'OIIIR']
     line_tbl_col = []
@@ -107,7 +106,7 @@ def indiv_em_table(fitspath, line_file):
         for nn in range(len(line_tbl_names)):
             line_tbl_col += [line_tbl_prefixes[mm] + line_tbl_names[nn]]
     
-    #Get column names for output table
+    # Get column names for output table
     cols = []
     line_names = [line_name[0], line_name[4], line_name[-1]]
     line_types = [line_type[0], line_type[4], line_type[-1]]
@@ -116,7 +115,7 @@ def indiv_em_table(fitspath, line_file):
     cols.remove('HBETA_Abs_Norm')
     cols.remove('HBETA_Abs_Sigma')
     
-    #Collect data in dictionary and write to output table
+    # Collect data in dictionary and write to output table
     line_table = hdu[1].data 
     line_table = line_table[idx_array]
     data = {indv_names0[0]:line_table['OBJNO'], indv_names0[3]:bin_npz['mass'][idx_array], 
@@ -125,4 +124,5 @@ def indiv_em_table(fitspath, line_file):
         data[cols[jj]] = line_table[line_tbl_col[jj]]
 
     out_ascii = fitspath + filename_dict['indv_prop']
-    asc.write(data, names=tuple([indv_names0[0]] + indv_names0[3:5] + cols), output=out_ascii, format='fixed_width_two_line', overwrite = True)
+    asc.write(data, names=tuple([indv_names0[0]] + indv_names0[3:5] + cols), output=out_ascii, 
+              format='fixed_width_two_line', overwrite = True)
