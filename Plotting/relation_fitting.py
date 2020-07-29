48184e91be4175c924d3bc0df757ce9676b1ca16
Caroline McCormick <cmccormick1@email.arizona.edu>
2020-07-29 15:17:54 -0700

    (PR #41) Deleted spaces next to equal signs for parameters in function calls.

--- Plotting/relation_fitting.py
+++ Plotting/relation_fitting.py
@@ -5,7 +5,7 @@ from Metallicity_Stack_Commons.column_names import npz_filename_dict, filename_d
 from Metallicity_Stack_Commons.column_names import temp_metal_names0, bin_names0
 
 
-def curve_fitting(x_array, y_array, restrict_MTO = False):
+def curve_fitting(x_array, y_array, restrict_MTO=False):
     '''
     Purpose:
         This function uses SciPy's curve_fit function to produce a curve that fits the composite Metallicity
@@ -15,12 +15,12 @@ def curve_fitting(x_array, y_array, restrict_MTO = False):
         x_array --> x values on plot (i.e. mass array).
         y_array --> y values on plot (i.e. metallicity array).
         restrict_MTO (OPTIONAL) --> if the mass turnover value should be held constant in the curve fit of
-                                    Metallicity vs Mass, then restrict_MTO = True.
+                                    Metallicity vs Mass, then restrict_MTO=True.
         
     Returns:
         o11 --> an array of the optimal values of the curve fit.
         o21 --> an array of the estimated covariance of o11.
-        fail --> if there is a ValueError when running curve_fit, then fail = True. Otherwise, fail = False.
+        fail --> if there is a ValueError when running curve_fit, then fail=True. Otherwise, fail=False.
     '''
         
     fail = False
@@ -31,7 +31,7 @@ def curve_fitting(x_array, y_array, restrict_MTO = False):
         p0 = [8.798, 0.640]
         para_bounds = ((8.0, 0.0), (9.0, 1.0))
     try:
-        o11, o21 = curve_fit(mass_metal_fit, x_array, y_array, p0 = p0, bounds = para_bounds)
+        o11, o21 = curve_fit(mass_metal_fit, x_array, y_array, p0=p0, bounds=para_bounds)
     except ValueError:
         print('Failed curve fitting!')
         fail = True
@@ -58,7 +58,7 @@ def AM13(mass):
 
 
     
-def mass_metal_fit(mass, a, g, b = 8.901):
+def mass_metal_fit(mass, a, g, b=8.901):
     '''
     Purpose:
         This function returns a curve calculated from best fit parameters from curve_fitting. The curve
@@ -92,8 +92,8 @@ def extract_error_bars(fitspath):
     '''
     
     der_prop_err = np.load(fitspath + npz_filename_dict['der_prop_errors'])
-    der_prop_file = asc.read(fitspath + filename_dict['bin_derived_prop_rev'])
-    valid_tbl = asc.read(fitspath + filename_dict['bin_valid_rev'], format = 'fixed_width_two_line')
+    der_prop_file = asc.read(fitspath + filename_dict['bin_derived_prop_rev'], format='fixed_width_two_line')
+    valid_tbl = asc.read(fitspath + filename_dict['bin_valid_rev'], format='fixed_width_two_line')
         
     Te_err = der_prop_err['T_e_lowhigh_error']        
     metal_err = der_prop_err['12+log(O/H)_lowhigh_error']
