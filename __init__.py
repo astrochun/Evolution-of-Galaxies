f5e616401b5bd2dccefa004abcb9a0f8f94eab1f
Chun Ly <astro.chun@gmail.com>
2021-01-19 22:27:26 -0700

    Implement logging in table_to_dict function

--- __init__.py
+++ __init__.py
@@ -1,18 +1,27 @@
 from astropy.io import ascii as asc
 
+from .log_commons import log_stdout
 
-def table_to_dict(tbl_name):
-    '''
-    Purpose: 
+
+def table_to_dict(tbl_name, log=None):
+    """
+    Purpose:
         This function takes an astropy ascii table and converts it to a dictionary.
-        
+
     Parameters:
         tbl_name --> a string containing the full file path of the table.
-        
+
     Returns:
-        A dictionary that contains key-value pairs of data column name to data array.  
-    '''
-    
+        A dictionary that contains key-value pairs of data column name to data array.
+    """
+
+    if log is None:
+        log = log_stdout()
+
+    log.info(f"starting ...")
+
+    log.info(f"Reading: {tbl_name}")
     tbl = asc.read(tbl_name)
-    
+
+    log.info("finished.")
     return tbl.to_pandas().to_dict(orient='list')
